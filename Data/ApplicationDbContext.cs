b38360254f63dc0a73831ad26eec292ea461d676
Guest User <Guest@epicodus-107.local>
2017-10-25 13:45:54 -0700

    initial

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserRolePractice.Models;
+
+namespace UserRolePractice.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		=> optionsBuilder
+			.UseMySql(@"Server=localhost;Port=8889;database=userrolepractice;uid=root;pwd=root;");
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UserRolePractice.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Zip { get; set; }
+		public string Email { get; set; }
+    }
+}
